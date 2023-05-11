429fbe40619d4740933ada9b7a5650075edad027
jashkarangiya <jashkarangiya@gmail.com>
2023-05-11 12:23:08 +0530

    Added new 3D Background Image Project

---
+++ my-three.js-learnings/3D-Animation Website/script.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+import './style.css';
+import bg1 from './img/bg1.jpg'
+import bg2 from './img/bg2.jpg'
+import { request } from 'http';
+
+
+
+const container = document.querySelector('.three_bg');
+const loader = new THREE.TextureLoader();
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+
+const geometry = new THREE.PlaneGeometry(16, 8, 15, 9);
+const material = new THREE.MeshBasicMaterial({
+    // color: #0xff000;
+    map: loader.load(bg2),
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+camera.position.z = 5;
+
+const count = geometry.attributes.position.count;
+const clock = new THREE.Clock()
+
+function animate() {
+    const time = clock.getElapsedTime();
+
+    for (let i = 0; i < count; i++) {
+
+        const x = geometry.attributes.position.getX(i);
+        const y = geometry.attributes.position.getY(i);
+
+        const anim1 = 0.25 * Math.sin(x + time * 0.7);
+        const anim2 = 0.35 * Math.sin(x * 1 + time * 0.7);
+        const anim3 = 0.1 * Math.sin(y * 15 + time * 0.7);
+
+
+
+        geometry.attributes.position.setZ(i, anim1 + anim2 + anim3);
+        geometry.computeVertexNormals();
+        geometry.attributes.position.needsUpdate = true;
+
+    }
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate()
